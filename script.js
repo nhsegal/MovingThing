7951a1975140cccfb2b301bd0f3d709abfe1df95
Segal <noah.segal@mbpr1317-3511.local>
2018-06-18 09:45:28 -0400

    set stage for motion

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ var speedSlider;
 
 var input1;
 var box1;
-
+var font,
+  fontsize = 40
 //Comment
 
+function preload(){
+  font = loadFont('assets/SourceSansPro-Regular.otf');
+}
+
 function setup() {
-  var cnv = createCanvas(800, 300);
+  var cnv = createCanvas(800, 200);
   cnv.parent("stage");
 }
 
 function draw() {
-  background(200);
+  background(50,150,50);
+  fill(0,200,250);
+  rect(0,0, 800, 150);
+  drawNumberLine();
 }
 
 function setSpeed1() {
 }
 
-function setSpeed2() {
+function drawNumberLine() {
+  fill(230,230,0);
+  rect(50,160,700,10);
 }
