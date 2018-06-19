e10bb0c2cc72c62554599b2218c16233de39b095
N <n.h.segal@gmail.com>
2018-06-19 15:13:19 -0400

    Recovering

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
-var font,fontsize = 40;
-var moverRad = 10;
-var xpos = 600;
-var ypos = 140;
-var vel = 2;
-var acc = -.050;
+var points = [];
+var t = 0;
 
-var points, seed, plot;
-//Comment
+var sketch = function(p){
+  p.moverRad = 10;
+  p.xpos = 500;
+  p.ypos = 140;
+  p.vel = 0;
+  p.acc = .000;
 
-//function preload(){
-//  font = loadFont('assets/SourceSansPro-Regular.otf');
-//}
-function setup() {
-  var cnv = createCanvas(1200, 500);
-  cnv.parent("stage");
-  points = [];
-		seed = 100 * random();
-
-		for (i = 0; i < 100; i++) {
-			points[i] = new GPoint(i, 10 * noise(0.1 * i + seed));
-		}
-
-		// Create a new plot and set its position on the screen
-		plot = new GPlot();
-		plot.setPos(0, 0);
-
-		// Add the points
-		plot.setPoints(points);
-
-		// Set the plot title and the axis labels
-		plot.setTitleText("A very simple example");
-		plot.getXAxis().setAxisLabelText("x axis");
-		plot.getYAxis().setAxisLabelText("y axis");
-
-		// Draw it!
-		plot.defaultDraw();
-
-
-}
-
-function draw() {
-  background(80,180,80);
-  fill(0,210,255);
-  rect(0,0, 1200, 150);
-  drawNumberLine();
-  updateMotion();
-  drawMover();
-  drawPosGraph();
-}
-
-function drawNumberLine() {
-  fill(230,230,0);
-  rect(50,160,1100,20);
-  fill(0);
-  textAlign(CENTER);
-  let i;
-  for (i = -10; i < 11; i++){
-    stroke(0);
-    text(i, 600+i*50+1,177);
-    line(600+i*50, 162, 600+i*50, 165);
+  p.setup  = function() {
+    p.createCanvas(1200, 200);
   }
-}
 
-function drawMover(){
-  ellipseMode(RADIUS);
-  fill(0)
-  ellipse(xpos, ypos, moverRad, moverRad);
-}
+  p.draw = function() {
+    p.background(80,180,80);
+    p.fill(0,210,255);
+    p.rect(0,0, 1200, 150);
+    drawNumberLine();
+    updateMotion();
+    drawMover();
+  }
 
-function updateMotion(){
-  vel = vel + acc;
-  xpos = xpos + vel;
-}
+  function drawNumberLine() {
+    p.fill(230,230,0);
+    p.rect(50,160,1100,20);
+    p.fill(0);
+    p.textAlign(p.CENTER);
+    for (let i = -10; i < 11; i++){
+      p.stroke(0);
+      p.text(i, 600+i*50+1,177);
+      p.line(600+i*50, 162, 600+i*50, 165);
+    }
+  }
 
-function drawPosGraph(){
-  fill(250);
-  rect(50,200,1100,200);
+  function drawMover(){
+    p.ellipseMode(p.RADIUS);
+    p.fill(0)
+    p.ellipse(p.xpos, p.ypos, p.moverRad, p.moverRad);
+  }
+
+  function updateMotion(){
+    p.vel = p.vel + p.acc;
+    p.xpos = p.xpos + p.vel;
+    points.push(p.xpos);
+    t = t+1;
+  };
 }
+var posGraph = function(p) {
+	p.setup = function() {
+    p.createCanvas(1200, 300);
+  }
+  p.draw = function() {
+    drawAxes();
+    plotPoints();
+  }
+
+  function drawAxes(){
+    p.stroke(0);
+    p.line(100,150,1100,150);
+    p.line(100,20,100,270);
+
+  }
+
+  function plotPoints(){
+    p.ellipseMode(p.CENTER);
+    p.noStroke();
+    p.fill(120,120,250);
+    var scalefactor = .02   //50 pixels to 1 meter
+    for (var i = 0; i < points.length; i++){
+      p.ellipse(2*i+100,-scalefactor*points[i]+162,4,4);
+
+    }
+    console.log(points);
+  }
+
+};
+
+var stage = new p5(sketch);
+var posGraph = new p5(posGraph);
